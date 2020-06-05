e6f971b5363939f4e24574cd7fdc179ac1c467c3
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2020-06-05 17:06:16 -0400

    use headers in processing

--- bookwarehouse/src/main/java/com/mogikanensoftware/bookwarehouse/order/service/OrderProcessingService.java
+++ bookwarehouse/src/main/java/com/mogikanensoftware/bookwarehouse/order/service/OrderProcessingService.java
@@ -11,16 +11,28 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class OrderProcessingService {
-    
+
     private JmsTemplate jmsTemplate;
 
     @Transactional(transactionManager = "jmsTransactionManager")
-    public void process(BookOrder bookOrder){
+    public void process(BookOrder bookOrder, String storeId, String orderState) {
         ProcessedBookOrder pbo = new ProcessedBookOrder(bookOrder, LocalDateTime.now(),
-         LocalDateTime.now().plusHours(48));
-         jmsTemplate.convertAndSend("processed-book-orders-queue", pbo);
+                LocalDateTime.now().plusHours(48));
+
+        if (orderState.equals("NEW")) {
+            this.add(bookOrder, storeId);
+        } else if (orderState.equals("UPDATE")) {
+            this.update(bookOrder, storeId);
+        } else {
+            throw new RuntimeException("Order State not supported->" + orderState);
+        }
+
+        jmsTemplate.convertAndSend("processed-book-orders-queue", pbo);
     }
 
     @Autowired
@@ -28,5 +40,11 @@ public class OrderProcessingService {
         this.jmsTemplate = jmsTemplate;
     }
 
-    
+    protected void add(BookOrder bookOrder, String storeId) {
+        log.info("Saving new order -> {}", bookOrder);
+    }
+
+    protected void update(BookOrder bookOrder, String storeId) {
+        log.info("Updating existing order -> {}", bookOrder);
+    }
 }
--- bookwarehouse/src/main/java/com/mogikanensoftware/bookwarehouse/order/service/OrderReceiver.java
+++ bookwarehouse/src/main/java/com/mogikanensoftware/bookwarehouse/order/service/OrderReceiver.java
@@ -5,6 +5,9 @@ import com.mogikanensoftware.bookwarehouse.order.model.BookOrder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -16,16 +19,19 @@ public class OrderReceiver {
     private OrderProcessingService orderProcessingService;
 
     @JmsListener(destination = "order-queue", containerFactory = "warehouseContainerFactory")
-    public void receive(BookOrder bookOrder) {
+    public void receive(@Payload BookOrder bookOrder,
+            @Header(name = "storeId") String storeId,
+            @Header(name = "orderState") String orderState,
+            MessageHeaders headers) {
 
         try {
-            log.info("Received order {}", bookOrder);
+            log.info("Received order {}, storeId->{}, orderState->{}, headers->{}", bookOrder, storeId, orderState,headers);
 
-            if(bookOrder.getBookOrderId().equals("000")){
+            if (bookOrder.getBookOrderId().equals("000")) {
                 throw new RuntimeException("invalid order 000");
             }
 
-            this.orderProcessingService.process(bookOrder);
+            this.orderProcessingService.process(bookOrder, storeId, orderState);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
